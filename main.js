693185a5b0f524c13a87cd00ec92f2bf56c3ec44
yavidor <yonatan@avidor.org>
2022-08-09 13:45:39 +0300

    I don't know at this point

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('BQCqOEEPxByx_EMN3KhoZNhHCA1Vl9oVLpDSvzZRamZ-x1NR49_mbMKhXpu1NqsjUAMb0jyCNZV4MNI6ShDXpfRtIa4V5mv3ekGIn0zuCd-fx6wrssRtOxMDs8OU4uvLAyKYZVs0hOo2v_CcCWzYcUn-Swa9eaUkpeekDDNDVXiQWyXOBj1yDPxFd6QVWrBrEK-AsDc1H38a7QruCkk3AgriQoZX-2DzBu6VGDRwuMXasq5s_JrSOyUWSIpkTihwQftMiDUuhrkPmMVQC5Y52zpXK5c_Il85IPBHr5INVuzJ')
+spotifyApi.setAccessToken('BQC8gllSCys0VmsOrF2My8xANF-sBnOP14oGtqHc4Pmbkf-WUFsElAVa9RTglr9M6YJ17oelafub8geYjTCKTFD1zbL5ilmoCY_JtCHVH53304HgVRZa5NnmoUOos1jhUMv7mq1388oeN--WRcptDwXWLB3as6ND4UI1JvcHMSe48SGFM-DsV4SLRmdUoCO0Q3SpSXDnkGbuBdY8X2K6crJ2fhERgoJJyJFB904qwarHAmqtKi99tpK3oLcYL3XjODVE9bUmlHtqT9MrnEJUWqVa0Q-rZcLoT6EYgJgDpMQj')
 function getPlaylists() {
-    spotifyApi.getUserPlaylists('3ycq8xooerqm1uh76g455t0c8').then(function (result) {
+    spotifyApi.getMySavedTracks('3ycq8xooerqm1uh76g455t0c8').then(function (result) {
         console.log(result);
-        for(let i = 0; i < result.items.length;i++){
-            document.write(`<img src="${result.items[i].images[0].url}" title="${result.items[i].name}" onclick="createPlay('3ycq8xooerqm1uh76g455t0c8','${result.items[i].id}','${result.items[i].name}','tempo')">`)
+        for (let i = 0; i < result.items.length; i++) {
+            document.write(`<img src="${result.items[i].track.album.images[0].url}" title="${result.items[i].track.name}" onclick="createPlay('3ycq8xooerqm1uh76g455t0c8','${result.items[i].id}','${result.items[i].name}','tempo')">`)
         }
     })
 }
-async function sortPlaylist(id){
-  return  getPlaylistWithTracks(id).then(async function (result) {
+async function sortPlaylist(id) {
+    return getPlaylistWithTracks(id).then(async function (result) {
         console.log('***', result);
         const ret = result.items;
         while (result.next) {
@@ -17,21 +17,22 @@ async function sortPlaylist(id){
             ret.push(...result.items)
         }
         urn = await getFeatures(ret.map(t => t.track.id), ret.map(t => t.track))
-        urn = urn.filter(x=>x.track.type=='track')
+        urn = urn.filter(x => x.track.type == 'track')
         songs = [];
         console.log(urn);
-        songs = urn.map(function(x){return {"name":x.track.id,"bpm":x.features.tempo}});
-        songs=songs.sort((a,b)=>a.bpm-b.bpm)
-        ids = songs.map(x=>x.name)
-        console.log(ids);   
+        songs = urn.map(function (x) { return { "name": x.track.id, "bpm": x.features.tempo } });
+        songs = songs.sort((a, b) => a.bpm - b.bpm)
+        ids = songs.map(x => x.name)
+        console.log(ids);
         console.log(songs);
-return ids;
-})}
-async function createPlay(uid,pid,pname,param){
+        return ids;
+    })
+}
+async function createPlay(uid, pid, pname, param) {
     ids = await sortPlaylist(pid);
-    ids = ids.map(x=>`spotify:track:${x}`).reverse();
-    spotifyApi.createPlaylist(uid,{name:`${pname} sorted by ${param}`}).then(function(result){
-        spotifyApi.addTracksToPlaylist(result.id,ids)
+    ids = ids.map(x => `spotify:track:${x}`).reverse();
+    spotifyApi.createPlaylist(uid, { name: `${pname} sorted by ${param}` }).then(function (result) {
+        spotifyApi.addTracksToPlaylist(result.id, ids)
     })
 }
 getPlaylists();
@@ -39,9 +40,10 @@ async function getPlaylistWithTracks(id, offset = 0) {
     // return await spotifyApi.getPlaylistTracks(id, {
     //     offset: offset
     // });
-    return await spotifyApi.getPlaylistTracks(id,{ limit: 50, offset: offset });
+    return await spotifyApi.getPlaylistTracks(id, { limit: 50, offset: offset });
 }
 async function getFeatures(ids, tracks) {
+
     songs = [];
     for (let i = 0; i < Math.ceil(ids.length / 50); i++) {
 
